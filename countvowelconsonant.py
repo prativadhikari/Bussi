867e7b3d1bf5e4eac2eda622fd7074a2213774fd
prativadhikari <prativadhikari1995@gmail.com>
2021-04-03 23:22:09 +0300

    'commit'

---
+++ countvowelconsonant.py
@@ -0,0 +1,20 @@
+"""
+The given program asks a user for a word and
+tells how many vowels and consonants the word contains.
+"""
+def main():
+    ask = input("Enter a word: ")
+    vowels = 0
+    consonants = 0
+    for i in ask:
+        if (i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u'):
+            vowels = vowels + 1
+        else:
+            consonants = consonants + 1
+
+    print("The word",(ask),"contains",(vowels),"vowels and",(consonants),"consonants")
+
+
+
+
+
